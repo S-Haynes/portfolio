b0aa96e522e866624aa9b72381646750e8078936
Shaquille Haynes <nothintodoee@yahoo.com>
2019-03-19 09:14:52 -0400

    add state to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,67 +1,97 @@
-import React from "react";
+import React, {Component} from "react";
 import { Link } from "react-router-dom";
 import { CardImg } from "reactstrap";
 import MiniLogo from "../../assets/img/minilogo.png";
 import "./Navbar.css";
-const Navbar = props => {
-  return (
-    <nav
-      style={{
-        height: "100%",
-        width: "80px",
-        position: "fixed",
-        zIndex: "100",
-        backgroundColor: "#181818",
-        top: "0",
-        left: "0"
-      }}
-      className="d-flex flex-column align-items-center"
-    >
-      <div className="mt-2">
-        <Link to="/">
-          <CardImg
-            style={{ width: "50px", height: "50px" }}
-            src={MiniLogo}
-            alt="logo"
-          />
-        </Link>
-      </div>
-      <div
-        style={{ width: "100%", height: "100%" }}
-        className="d-flex flex-column justify-content-center align-items-center"
+class Navbar extends Component {
+
+  state = {
+    home: false,
+    about: false,
+    skills: false,
+    work: false,
+    contact: false
+  }
+  
+  onNavClick = (e) => {
+
+  let updatedState = {...this.state}
+
+  for(let key in updatedState){
+    updatedState[key] = false
+  }
+
+  this.setState(prevState => updatedState)
+
+  const attr = e.target.getAttribute('customattribute')
+
+  this.setState(prevState => ({[attr]: !prevState[attr]}))
+
+}
+
+  
+  render(){
+
+    return (
+      <nav
+        style={{
+          height: "100%",
+          width: "80px",
+          position: "fixed",
+          zIndex: "100",
+          backgroundColor: "#181818",
+          top: "0",
+          left: "0"
+        }}
+        className="d-flex flex-column align-items-center"
       >
-        <Link className="mt-4" to="/">
-          <i className="fas fa-home" />
-        </Link>
-        <Link className="mt-4" to="/about">
-          <i className="far fa-user" />
-        </Link>
-        <Link className="mt-4" to="/skills">
-          <i className="fas fa-cogs" />
-        </Link>
-        <Link className="mt-4" to="/work">
-          <i className="far fa-eye" />
-        </Link>
-        <Link className="mt-4" to="/contact">
-          <i className="far fa-envelope" />
-        </Link>
-      </div>
-      <div
-        style={{ width: "100%", height: "10%" }}
-        className="d-flex flex-column justify-content-end align-items-center"
-      >
-        <a className="mt-4" href="/">
-          <i className="fab fa-linkedin-in" />
-        </a>
-        <a className="mt-4" href="https://www.codepen.io/s-haynes">
-          <i className="fab fa-codepen" />
-        </a>
-        <a className="mt-4 mb-4" href="https://www.github.com/s-haynes">
-          <i className="fab fa-github" />
-        </a>
-      </div>
-    </nav>
-  );
-};
+        <div className="mt-2">
+          <Link to="/">
+            <CardImg
+              style={{ width: "50px", height: "50px" }}
+              src={MiniLogo}
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <div
+          style={{ width: "100%", height: "100%" }}
+          className="d-flex flex-column justify-content-center align-items-center"
+        >
+          <Link name="home" className="mt-4" to="/">
+            <i onClick={this.onNavClick} style={this.state.home ? {color: '#08fdd8'} : null}  customattribute="home" className="fas fa-home" />
+          </Link>
+          <Link name="about" className="mt-4" to="/about">
+            <i onClick={this.onNavClick} style={this.state.about ? {color: '#08fdd8'} : null} customattribute="about" className="far fa-user" />
+          </Link>
+          <Link name="skills" className="mt-4" to="/skills">
+            <i onClick={this.onNavClick} style={this.state.skills ? {color: '#08fdd8'} : null}  customattribute="skills" className="fas fa-cogs" />
+          </Link>
+          <Link  className="mt-4" to="/work">
+            <i onClick={this.onNavClick} style={this.state.work ? {color: '#08fdd8'} : null} customattribute="work" className="far fa-eye" />
+          </Link>
+          <Link className="mt-4" to="/contact">
+            <i onClick={this.onNavClick} style={this.state.contact ? {color: '#08fdd8'} : null} customattribute="contact" className="far fa-envelope" />
+          </Link>
+        </div>
+        <div
+          style={{ width: "100%", height: "10%" }}
+          className="d-flex flex-column justify-content-end align-items-center"
+        >
+          <a className="mt-4" href="/">
+            <i className="fab fa-linkedin-in" />
+          </a>
+          <a className="mt-4" href="https://www.codepen.io/s-haynes">
+            <i className="fab fa-codepen" />
+          </a>
+          <a className="mt-4 mb-4" href="https://www.github.com/s-haynes">
+            <i className="fab fa-github" />
+          </a>
+        </div>
+      </nav>
+    );
+  };
+  }
+  
 
 export default Navbar;
