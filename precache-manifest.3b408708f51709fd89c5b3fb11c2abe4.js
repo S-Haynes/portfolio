1fa1487a1fa4c56f1b1e958188e09dcfb0f9947d
Shaquille Haynes <nothintodoee@yahoo.com>
2019-03-23 04:50:53 -0400

    Updates

---
+++ precache-manifest.3b408708f51709fd89c5b3fb11c2abe4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "6450172cfde32885cfaf1fed2ca511a7",
+    "url": "/portfolio_v2/static/media/work3tech.6450172c.png"
+  },
+  {
+    "revision": "d4f5793b45b710d05a83",
+    "url": "/portfolio_v2/static/css/main.46a08db1.chunk.css"
+  },
+  {
+    "revision": "c7316f2c06c9c7b62066",
+    "url": "/portfolio_v2/static/js/runtime~main.a7a80610.js"
+  },
+  {
+    "revision": "c53948fa2729b4822f8fb0a49ba891a9",
+    "url": "/portfolio_v2/static/media/experiment10.c53948fa.png"
+  },
+  {
+    "revision": "3a628231009585e31912",
+    "url": "/portfolio_v2/static/js/2.8b5022a3.chunk.js"
+  },
+  {
+    "revision": "85b399bb0823b91243952997ebf031b1",
+    "url": "/portfolio_v2/static/media/logo.85b399bb.png"
+  },
+  {
+    "revision": "ac8d8c12ce92ae811751d48798152881",
+    "url": "/portfolio_v2/static/media/minilogo.ac8d8c12.png"
+  },
+  {
+    "revision": "f1f48c91d8e8299009cdb785377b6fe3",
+    "url": "/portfolio_v2/static/media/timeline.f1f48c91.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio_v2/static/media/react-logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "65a64a139228e1bb71d605ee3ffe9310",
+    "url": "/portfolio_v2/static/media/work1tech.65a64a13.png"
+  },
+  {
+    "revision": "171ea45aeb1cfa3d22400a64db80e5c7",
+    "url": "/portfolio_v2/static/media/work2tech.171ea45a.png"
+  },
+  {
+    "revision": "d4f5793b45b710d05a83",
+    "url": "/portfolio_v2/static/js/main.aa46c364.chunk.js"
+  },
+  {
+    "revision": "eac8ffab68eb5920aa2e84b08ae8fa09",
+    "url": "/portfolio_v2/static/media/work4tech.eac8ffab.png"
+  },
+  {
+    "revision": "f63a56971de224bc4f375e5dad00b699",
+    "url": "/portfolio_v2/static/media/experiment1.f63a5697.png"
+  },
+  {
+    "revision": "0e89a48034ed0bd58a5b5fed2b3c673d",
+    "url": "/portfolio_v2/static/media/experiment2.0e89a480.png"
+  },
+  {
+    "revision": "509992e78bf42c25f8ce72c3f43068b3",
+    "url": "/portfolio_v2/static/media/experiment3.509992e7.png"
+  },
+  {
+    "revision": "5a963a45f531c92606da22d1ddbc6919",
+    "url": "/portfolio_v2/static/media/experiment4.5a963a45.png"
+  },
+  {
+    "revision": "003b175447761bdfd1bb71eb841b6159",
+    "url": "/portfolio_v2/static/media/experiment6.003b1754.png"
+  },
+  {
+    "revision": "d284cf48ad657450dad230e6c4661dd5",
+    "url": "/portfolio_v2/static/media/experiment7.d284cf48.png"
+  },
+  {
+    "revision": "0d0f5bc3bd82e298e05e4d4c7ab3fbd0",
+    "url": "/portfolio_v2/static/media/experiment9.0d0f5bc3.png"
+  },
+  {
+    "revision": "3a628231009585e31912",
+    "url": "/portfolio_v2/static/css/2.2a757927.chunk.css"
+  },
+  {
+    "revision": "60e20d9e5b12f98694cc7b07a377921b",
+    "url": "/portfolio_v2/index.html"
+  }
+];
